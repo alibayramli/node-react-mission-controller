d972b4881d20eae6f57c0b8a84b65456b4d5ec9d
Ali Bayramli <bayramliali@tutanota.com>
2021-12-10 10:11:35 +0100

    Add secrets to dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URL =
-  "mongodb+srv://nasa-api:cKTMteNNJCen4dCj@nasacluster.xxb6p.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection is ready!");
